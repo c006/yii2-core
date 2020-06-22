b6e8a866c9cb3c1e160d71998e6767516e72e253
Jon Chambers <jchambers.dev@gmail.com>
2020-06-22 15:48:35 -0700

    2020 / June - Updates

--- assets/CoreHelper.php
+++ assets/CoreHelper.php
@@ -398,6 +398,28 @@ class CoreHelper
 
 
     /**
+     * array_orderby($data, 'volume', SORT_DESC, 'edition', SORT_ASC);
+     *
+     * @return mixed
+     */
+    static public function array_orderby()
+    {
+        $args = func_get_args();
+        $data = array_shift($args);
+        foreach ($args as $n => $field) {
+            if (is_string($field)) {
+                $tmp = [];
+                foreach ($data as $key => $row)
+                    $tmp[$key] = $row[$field];
+                $args[$n] = $tmp;
+            }
+        }
+        $args[] = &$data;
+        call_user_func_array('array_multisort', $args);
+        return array_pop($args);
+    }
+
+    /**
      * @param $model_class
      * @param $array
      *
